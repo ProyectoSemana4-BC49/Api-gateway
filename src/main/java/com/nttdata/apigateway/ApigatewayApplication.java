6e0fcb03dd0ebffb7642e44bf6c2456f65740689
Klevert Herrera <vertkle@gmail.com>
2024-02-13 15:02:13 -0500

    deply

--- src/main/java/com/nttdata/apigateway/ApigatewayApplication.java
+++ src/main/java/com/nttdata/apigateway/ApigatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApigatewayApplication {
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
